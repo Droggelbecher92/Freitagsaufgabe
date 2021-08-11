f57540897865e2a98e19323039bcfcf3fab0d27f
Stefan Lautner <stefan.lautner@highpace.de>
2021-08-11 12:54:56 +0200

    add backend intelliJ spring init

---
+++ backend/src/main/java/de/neuefische/rem_213_github/backend/Rem213GithubApplication.java
@@ -0,0 +1,13 @@
+package de.neuefische.rem_213_github.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Rem213GithubApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Rem213GithubApplication.class, args);
+    }
+
+}
