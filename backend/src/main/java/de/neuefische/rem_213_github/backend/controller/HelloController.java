32a888f2f04e698054b5609882c79ecddeeeecbc
Stefan Lautner <stefan.lautner@highpace.de>
2021-08-11 16:24:44 +0200

    add swagger link

--- backend/src/main/java/de/neuefische/rem_213_github/backend/controller/HelloController.java
+++ backend/src/main/java/de/neuefische/rem_213_github/backend/controller/HelloController.java
@@ -7,6 +7,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+import springfox.documentation.annotations.ApiIgnore;
 
 import static de.neuefische.rem_213_github.backend.controller.HelloController.HELLO_TAG;
 import static org.springframework.http.ResponseEntity.ok;
@@ -21,10 +22,15 @@ public class HelloController {
 
     public static final String HELLO_TAG = "Hello";
 
-    @ApiOperation("Say hello to REM 21-3")
+    @ApiOperation(value = "Say hello to REM 21-3", hidden = true)
     @GetMapping("/")
     public ResponseEntity<String> hello() {
         String cssStyle = "position: fixed; top: 50%; left: 50%; transform: translate(-50%, -50%);";
-        return ok(String.format("<html><body><h1 style=\"%s\">Hello REM 21-3</h1></body></html>", cssStyle));
+        String swaggerLink = "/api/rem213/swagger-ui/";
+
+        return ok(String.format(
+                "<html><body><h1 style=\"%s\"><a href=\"%s\" alt=\"\">Swagger REM 21-3</a></h1></body></html>",
+                cssStyle, swaggerLink
+        ));
     }
 }
